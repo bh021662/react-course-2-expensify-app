1eb6bb5144b143ba8885c245e50148a21d1e9435
Henson <BH021662@cerner.net>
2018-11-25 20:43:12 -0600

    Setup test database env changes 2

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = (env) => {
         },
         plugins: [
             CSSExtract,
-            new webpack.DefinePlugIn({
+            webpack.DefinePlugIn({
                 'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
                 'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
                 'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
